6d9cdba40ca95d7da6630f682780e8c20c68c74f
MD ANAS <91305027+NARUTO3251@users.noreply.github.com>
2025-03-10 00:37:28 +0530

    Create Q12_Solution.java

---
+++ JAVA_ASSIGNMENTS/Assignment_2/Q12_Solution.java
@@ -0,0 +1,12 @@
+public class Solution{
+	public static void main(String[] args){
+		check(7);
+		check(20);
+		check(-9);
+	}
+	
+	public static void check(int a){
+		int result = a << 3;
+		System.out.println(result);
+	} 
+}	
