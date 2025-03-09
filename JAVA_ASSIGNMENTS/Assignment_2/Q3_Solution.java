fd04cc3e21584e876a063d68f7b780ba1c217262
MD ANAS <91305027+NARUTO3251@users.noreply.github.com>
2025-03-10 00:25:09 +0530

    Create Q3_Solution.java

---
+++ JAVA_ASSIGNMENTS/Assignment_2/Q3_Solution.java
@@ -0,0 +1,11 @@
+public class Solution{
+	public static void main(String[] args){
+		int x = 123456;
+		int sum = 0;
+		while(x!=0){
+			sum = sum + (x%10);
+			x = x/10;
+		}
+		System.out.println("Sum: " + res);
+	}
+}
